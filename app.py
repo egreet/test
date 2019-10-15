c3890f291a3b657484d4c35df7c5248c8ac09ead
KHANDELWAL Deepanshu <deepanshu.khandelwal@soprasteria.com>
2019-10-15 15:16:34 +0530

    done

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,render_templates
 
 app=Flask(__name__)
 
 @app.route('/')
 def index():
-	return "HI"
+	return render_templates('index.html')
 
 
 if __name__=="__main__":
